1bcb96ba1ac2cd8f7ec58084bdcd2180b134163a
Arjun Telange <arjuntelange08@gmail.com>
2026-04-14 12:37:30 +0530

    Practiced arithmetic operators in js

---
+++ operators/arithmetic.js
@@ -0,0 +1,8 @@
+let a=90;
+let b=50;
+
+console.log("Addition= ", (a+b));
+console.log("Subtraction= ", (a-b));
+console.log("Multiplication= ", (a*b));
+console.log("Division= ",(a/b));
+console.log("Modulus= ", (a%b));
