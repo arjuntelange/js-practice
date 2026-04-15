9ccc9e7a877ab46db5db99a68d5e2d5dd5a28ce3
Arjun Telange <arjuntelange08@gmail.com>
2026-04-15 21:30:42 +0530

    Practiced datatypes in JS

---
+++ datatypes/non-primitive/array.js
@@ -0,0 +1,8 @@
+let arr= [10, 20, 30, 40];
+console.log(arr);
+console.log("Data type: ",typeof arr);
+
+// Array declaration using constructor
+
+let arr2 = new Array(50, 40, 90);
+console.log(arr2);
---
+++ datatypes/non-primitive/object.js
@@ -0,0 +1,16 @@
+let obj= {
+    name: "Arjun",
+    age: 19,
+    gender: "Male"
+}
+console.log(obj);
+console.log("Data type: ", typeof obj);
+
+// Object declaration using constructor
+
+let obj2 = new Object();
+obj2.name= "Sanskruti";
+obj2.age= 18;
+obj2.gender = "Female";
+
+console.log(obj2);
