95869c9cfa2c7a90d2bf00c624ae682421fea2ef
Arjun Telange <arjuntelange08@gmail.com>
2026-04-16 22:08:25 +0530

    Practiced non primitive data types in js

--- datatypes/non-primitive/object.js
+++ datatypes/non-primitive/object.js
@@ -12,5 +12,4 @@ let obj2 = new Object();
 obj2.name= "Sanskruti";
 obj2.age= 18;
 obj2.gender = "Female";
-
 console.log(obj2);
--- datatypes/symbol.js
+++ datatypes/symbol.js
@@ -1,4 +1,3 @@
 let id = Symbol("id");
 console.log(id);
 console.log("Data type: ", typeof id);
-
