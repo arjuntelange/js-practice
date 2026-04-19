078053b45289047c837dcbaf2c12bd23fdf02138
Arjun Telange <arjuntelange08@gmail.com>
2026-04-19 21:32:27 +0530

    Practiced functions in Js

---
+++ functions/parameter-function.js
@@ -0,0 +1,5 @@
+function greet(f_name) {
+    console.log("Congratulations ", f_name,"!");
+}
+let your_name= "Arjun";
+greet(your_name);
---
+++ functions/return-function.js
@@ -0,0 +1,9 @@
+function sub(a, s) {
+    return a-s;
+}
+
+let num1= 90;
+let num2= 50;
+
+result = sub(num1, num2);
+console.log(result);
