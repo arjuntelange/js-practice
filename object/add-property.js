dd14383f4b3e66a7939f1b6092fd20480f87a2a9
Arjun Telange <arjuntelange08@gmail.com>
2026-05-12 23:42:43 +0530

    practice object related topics

---
+++ object/add-property.js
@@ -0,0 +1,8 @@
+let student = {
+    name: "Arjun",
+    age: 19,
+}
+console.log(student);
+
+student.city = "Latur";
+console.log(student);
---
+++ object/del-property.js
@@ -0,0 +1,9 @@
+let student = {
+    name: "Arjun",
+    age: 19,
+    city: "Latur"
+}
+console.log(student);
+
+delete student.city;
+console.log(student);
