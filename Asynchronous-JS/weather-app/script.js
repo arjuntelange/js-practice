286ff7fc930c9b84193b9f01378b6e0362930730
Arjun Telange <arjuntelange08@gmail.com>
2026-06-03 18:58:27 +0530

    feat: integrate weather API and display live weather data

--- Asynchronous-JS/weather-app/script.js
+++ Asynchronous-JS/weather-app/script.js
@@ -8,7 +8,31 @@ const humidity = document.getElementById("humidity");
 
 async function getWeather() {
   const city = cityInput.value;
-  console.log(city);
+  if (!city) {
+    alert("Please enter a city name");
+    return;
+  }
+
+  temperature.textContent = "Loading...";
+  cityName.textContent = "";
+  weatherCondition.textContent = "";
+  humidity.textContent = "";
+
+  const response = await fetch(`https://wttr.in/${city}?format=j1`);
+  const data = await response.json();
+
+  temperature.textContent = `🌤️ ${data.current_condition[0].temp_C}°C`;
+  cityName.textContent = `📍${city}`;
+  weatherCondition.textContent = data.current_condition[0].weatherDesc[0].value;
+  humidity.textContent = `Humidity: ${data.current_condition[0].humidity}%`;
+
+  cityInput.value = "";
 }
 
 searchBtn.addEventListener("click", getWeather);
+
+cityInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    getWeather();
+  }
+});
